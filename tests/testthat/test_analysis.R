ff08b36de4ee143f5798d2c4e6beb5638d1eb1b2
JLDC <jonathan.chassot@unisg.ch>
2020-06-22 14:43:09 +0200

    Fix deprecated test

--- tests/testthat/test_analysis.R
+++ tests/testthat/test_analysis.R
@@ -1,10 +1,10 @@
 # Test the expected output of a salary analysis using the internal test data
 test_that(paste0("analysis() returns an object of type ",
                  "analysis_model"), {
-                   results <- standard_analysis(test_data, 2019, 2, 1)
+                   results <- analysis(test_data, 1, 2019, 2, 1)
                    expect_s3_class(results, "analysis_model")
                  })
 test_that("analysis() returns a list object", {
-  results <- analysis(test_data, 2019, 2, 1)
+  results <- analysis(test_data, 1, 2019, 2, 1)
   expect_type(results, "list")
 })
